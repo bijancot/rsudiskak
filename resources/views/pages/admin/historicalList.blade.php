7f67de0a3554da4d68466f6830618ef91f423a26
Ade Ramadhana Pratama <171111020@mhs.stiki.ac.id>
2020-11-27 19:35:25 +0700

    fix bug search

--- resources/views/pages/admin/historicalList.blade.php
+++ resources/views/pages/admin/historicalList.blade.php
@@ -8,9 +8,10 @@
         <div class="wrapper">
             <div class="search-box-box">
                 <form class="form form-inline">
-                    @csrf
                     <input id="cstm_search" type="search" placeholder="Cari Nama Pasien" class="soft-shadow">
-                    <span><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.9167 11.6667H12.2583L12.025 11.4417C12.8417 10.4917 13.3333 9.25833 13.3333 7.91667C13.3333 4.925 10.9083 2.5 7.91667 2.5C4.925 2.5 2.5 4.925 2.5 7.91667C2.5 10.9083 4.925 13.3333 7.91667 13.3333C9.25833 13.3333 10.4917 12.8417 11.4417 12.025L11.6667 12.2583V12.9167L15.8333 17.075L17.075 15.8333L12.9167 11.6667ZM7.91667 11.6667C5.84167 11.6667 4.16667 9.99167 4.16667 7.91667C4.16667 5.84167 5.84167 4.16667 7.91667 4.16667C9.99167 4.16667 11.6667 5.84167 11.6667 7.91667C11.6667 9.99167 9.99167 11.6667 7.91667 11.6667Z" fill="#C7D3CC"/></svg></span>
+                    <span><svg style="margin-top:-20px;" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.9167 11.6667H12.2583L12.025 11.4417C12.8417 10.4917 13.3333 9.25833 13.3333 7.91667C13.3333 4.925 10.9083 2.5 7.91667 2.5C4.925 2.5 2.5 4.925 2.5 7.91667C2.5 10.9083 4.925 13.3333 7.91667 13.3333C9.25833 13.3333 10.4917 12.8417 11.4417 12.025L11.6667 12.2583V12.9167L15.8333 17.075L17.075 15.8333L12.9167 11.6667ZM7.91667 11.6667C5.84167 11.6667 4.16667 9.99167 4.16667 7.91667C4.16667 5.84167 5.84167 4.16667 7.91667 4.16667C9.99167 4.16667 11.6667 5.84167 11.6667 7.91667C11.6667 9.99167 9.99167 11.6667 7.91667 11.6667Z" fill="#C7D3CC"/></svg></span>
+                    
+                    @csrf
                     &nbsp;
                     <input type="date" id="date" onchange="handler(event);" class="form-control">&nbsp;
                     <!-- <input type="button" id="submit"  class="form-control btn btn-primary" value="Filter"> -->
@@ -56,7 +57,7 @@
             $('#cstm_search').on( 'keyup search input paste cut', function () {
                 table.search(this.value).draw();
             });
-            //$('#tbl_Riwayat_filter').hide();
+            $('#tbl_Riwayat_filter').hide();
         });
         
     </script>
@@ -74,11 +75,11 @@
             success : function(data){
                 $('#table1').html(data.html);
                 $('#cstm_search').on("keyup search input paste cut", function () {
-                    table.search(this.value).draw();
+                    $('#tbl_Riwayat').DataTable().rows('.odd').search(this.value).draw();
                     //$('#tbl_listPasienKirimPoli').hasClass('odd').draw();
                 });
                 $("#tbl_Riwayat").dataTable();
-                //$('#tbl_Riwayat_filter').hide();
+                $('#tbl_Riwayat_filter').hide();
             },
             error: function (request, status, error) {
                 //alert("error");
