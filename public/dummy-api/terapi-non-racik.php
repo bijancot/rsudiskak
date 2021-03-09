fefe4947b11759347a0eaf35270280d5fa5cadb7
Panji Iman Baskoro <panjidia995@gmail.com>
2021-03-09 14:36:34 +0700

    add terapi racikan

--- public/dummy-api/terapi-non-racik.php
+++ public/dummy-api/terapi-non-racik.php
@@ -2,7 +2,7 @@
     $json = array(
         'meta' => array('code' => "200", 'description' => "success"),
         'response' => array(
-            'data' => array(array('nama_obat' => 'Furosemide', 'dosis' => '40', 'qty' => '30', 'signa' => array('pagi' => '1/2', 'siang' => '0', 'malam' => '0'), 'keterangan' => ''), array('nama_obat' => 'Furosemide', 'dosis' => '40', 'qty' => '30', 'signa' => array('pagi' => '1/2', 'siang' => '0', 'malam' => '0'), 'keterangan' => '')), 'first_page_url' => "string", 'from' => "1", 'last_page' => "3", 'last_page_url' => "string", 'next_page_url' => "string", 'path' => "string", 'per_page' => "20", 'prev_page_url' => NULL, 'to' => "20", 'total' => "45",
+            'data' => array(array('nama_obat' => 'Furosemide', 'dosis' => '40', 'qty' => '30', 'signa' => array('pagi' => '1/2', 'siang' => '0', 'malam' => '0'), 'keterangan' => ''), array('nama_obat' => 'Captopril', 'dosis' => '12.5', 'qty' => '90', 'signa' => array('pagi' => '1/2', 'siang' => '5', 'malam' => '0'), 'keterangan' => 'Yuhuu')), 'first_page_url' => "string", 'from' => "1", 'last_page' => "3", 'last_page_url' => "string", 'next_page_url' => "string", 'path' => "string", 'per_page' => "20", 'prev_page_url' => NULL, 'to' => "20", 'total' => "45",
         )
     );
     header('Content-Type: application/json');
---
+++ public/dummy-api/terapi-racik.php
@@ -0,0 +1,10 @@
+    <?php
+    $json = array(
+        'meta' => array('code' => "200", 'description' => "success"),
+        'response' => array(
+            'data' => array(array('nama_obat' => 'Furosemide', 'dosis' => '40', 'racikan_dalam' => 'Pulv(Puyer)/ Zalf(Salep)/ Cr (Cream)/ Oin(oinment)', 'qty' => '30', 'signa' => array('pagi' => '1/2', 'siang' => '0', 'malam' => '0'), 'keterangan' => ''), array('nama_obat' => 'Captopril', 'dosis' => '12.5', 'racikan_dalam' => 'Pulv(Puyer)/ Zalf(Salep)/ Cr (Cream)/ Oin(oinment)', 'qty' => '90', 'signa' => array('pagi' => '1/2', 'siang' => '5', 'malam' => '0'), 'keterangan' => 'Yuhuu')), 'first_page_url' => "string", 'from' => "1", 'last_page' => "3", 'last_page_url' => "string", 'next_page_url' => "string", 'path' => "string", 'per_page' => "20", 'prev_page_url' => NULL, 'to' => "20", 'total' => "45",
+        )
+    );
+    header('Content-Type: application/json');
+    print_r(json_encode($json));
+    ?>
