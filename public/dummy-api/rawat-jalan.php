5a291b97b313a31b864524ff4e6db6a5ba08ecc4
Panji Iman Baskoro <panjidia995@gmail.com>
2021-03-14 22:49:08 +0700

    add verifikasi

--- public/dummy-api/rawat-jalan.php
+++ public/dummy-api/rawat-jalan.php
@@ -2,7 +2,7 @@
     $json = array(
         'meta' => array('code' => "200", 'description' => "success"),
         'response' => array(
-            'data' => array(array('NoCM' => '11608050', 'KdRuangan' => '215', 'TglBerkunjung' => '2016-03-10 00:00:00', 'Poliklinik' => 'poli jantung', 'Diagnosis' => 'CAD,HF', 'TatalaksanaTerapi' => 'Bendzodiazepin'), array('NoCM' => '11608050', 'KdRuangan' => '215', 'TglBerkunjung' => '2016-03-10 00:00:00', 'Poliklinik' => 'poli jantung', 'Diagnosis' => 'CAD,HF', 'TatalaksanaTerapi' => 'Furosemide 1x40 mg')), 'first_page_url' => "string", 'from' => "1", 'last_page' => "3", 'last_page_url' => "string", 'next_page_url' => "string", 'path' => "string", 'per_page' => "20", 'prev_page_url' => NULL, 'to' => "20", 'total' => "45",
+            'data' => array(array('NoCM' => '11608050', 'KdRuangan' => '215', 'TglBerkunjung' => '2016-03-10 00:00:00', 'Poliklinik' => 'poli jantung', 'Diagnosis' => 'CAD,HF', 'TatalaksanaTerapi' => 'Bendzodiazepin', 'VerifikasiPetugas' => '2'), array('NoCM' => '11608050', 'KdRuangan' => '215', 'TglBerkunjung' => '2016-03-10 00:00:00', 'Poliklinik' => 'poli jantung', 'Diagnosis' => 'CAD,HF', 'TatalaksanaTerapi' => 'Furosemide 1x40 mg', 'VerifikasiPetugas' => '2')), 'first_page_url' => "string", 'from' => "1", 'last_page' => "3", 'last_page_url' => "string", 'next_page_url' => "string", 'path' => "string", 'per_page' => "20", 'prev_page_url' => NULL, 'to' => "20", 'total' => "45",
         )
     );
     header('Content-Type: application/json');
